ca76cc1a10c290262135330e3aaf82ee2c0e2112
kiki-kanri <a470666@gmail.com>
2022-11-27 22:47:56 +0800

    Add packages and setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,60 @@
+import removeConsole from 'vite-plugin-remove-console';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', href: '/favicon.svg' }
+			],
+			noscript: [
+				{ children: 'Javascript is required.' }
+			]
+		},
+		keepalive: true
+	},
+	compression: {
+		viteCompression: {
+			algorithm: 'gzip',
+			threshold: 513
+		}
+	},
+	css: [
+	],
+	imports: {
+		dirs: [
+			'composables',
+			'composables/*/*.{ts,js,mjs,mts}'
+		]
+	},
+	modules: [
+		'@averjs/nuxt-compression',
+		'nuxt-purgecss'
+	],
+	purgecss: {
+		enabled: true,
+		safelist: {
+			deep: [
+			],
+			standard: [
+			]
+		}
+	},
+	ssr: false,
+	vite: {
+		build: {
+			chunkSizeWarningLimit: 1024,
+			manifest: false,
+			rollupOptions: {
+				output: {
+					manualChunks: {
+					}
+				}
+			},
+			ssr: false
+		},
+		plugins: [
+			removeConsole()
+		]
+	}
+});
