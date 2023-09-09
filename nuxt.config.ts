97f8102ce8cf8d19ff00f68ca2c8b40922bed631
kiki-kanri <a470666@gmail.com>
2023-09-09 22:35:16 +0800

    feat: use nuxt config devServer set dev host and port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,10 @@ export default defineNuxtConfig({
 		},
 		keepalive: true
 	},
+	devServer: {
+		host: process.env.SERVER_HOST,
+		port: Number(process.env.SERVER_PORT) || undefined
+	},
 	experimental: {
 		payloadExtraction: false
 	},
--- run-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. ./.env
-
-npx nuxt dev --host=$SERVER_HOST --port=$SERVER_PORT
